9a78e2d31a4fad48ee825cbf3bb0127cca0e385a
Neander Marcel Heming <neanderh@yahoo.com.br>
2024-10-22 12:10:02 +0000

    version 0.7.1

--- inst/doc/SES.R
+++ inst/doc/SES.R
@@ -1,10 +1,10 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ---- rand-spp, fig.height=4, fig.width=4-------------------------------------
+## ----rand-spp, fig.height=4, fig.width=4--------------------------------------
 library(SESraster)
 library(terra)
 # creating random species distributions
@@ -23,24 +23,24 @@ r <- rast(lapply(1:18,
 names(r) <- paste("sp", 1:nlyr(r))
 plot(r)
 
-## ---- ses-fun-----------------------------------------------------------------
+## ----ses-fun------------------------------------------------------------------
 appmean <- function(x, ...){
                       terra::app(x, "mean", ...)
 }
 
-## ---- ses-spat-sp, fig.height=4, fig.width=4----------------------------------
+## ----ses-spat-sp, fig.height=4, fig.width=4-----------------------------------
 ses.sp <- SESraster(r, FUN = appmean, 
                     spat_alg = "bootspat_naive", spat_alg_args = list(random = "species"),
                     aleats = 5)
 plot(ses.sp)
 
-## ---- ses-spat-st, fig.height=4, fig.width=4----------------------------------
+## ----ses-spat-st, fig.height=4, fig.width=4-----------------------------------
 ses.st <- SESraster(r, FUN = appmean,
                     spat_alg = "bootspat_naive", spat_alg_args = list(random = "site"),
                     aleats = 5)
 plot(ses.st)
 
-## ---- ses-spat-na1, fig.height=4, fig.width=4---------------------------------
+## ----ses-spat-na1, fig.height=4, fig.width=4----------------------------------
 ## let's create some missing values for layer/species 1
 r2 <- r
 set.seed(10)
@@ -54,7 +54,7 @@ sesNA <- SESraster(r2, FUN = appmean, FUN_args = list(na.rm = FALSE),
 head(sesNA[cellsNA])
 plot(sesNA)
 
-## ---- ses-spat-na2, fig.height=4, fig.width=4---------------------------------
+## ----ses-spat-na2, fig.height=4, fig.width=4----------------------------------
 set.seed(10)
 ses.woNA <- SESraster(r2, FUN = appmean, FUN_args = list(na.rm = TRUE), 
                       spat_alg = "bootspat_naive", spat_alg_args=list(random = "species"),
@@ -62,7 +62,7 @@ ses.woNA <- SESraster(r2, FUN = appmean, FUN_args = list(na.rm = TRUE),
 head(ses.woNA[cellsNA])
 plot(ses.woNA)
 
-## ---- ses-ftrait, fig.height=4, fig.width=4-----------------------------------
+## ----ses-ftrait, fig.height=4, fig.width=4------------------------------------
 ## example with `Fa_alg`
 appsv <- function(x, lyrv, na.rm = FALSE, ...){
                       sumw <- function(x, lyrv, na.rm, ...){
@@ -76,7 +76,7 @@ set.seed(10)
 trait  <- sample(100:2000, nlyr(r))
 trait
 
-## ---- ses-trait1, fig.height=4, fig.width=4-----------------------------------
+## ----ses-trait1, fig.height=4, fig.width=4------------------------------------
 set.seed(10)
 ses <- SESraster(r, FUN = appsv,
                  FUN_args = list(lyrv = trait, na.rm = TRUE),
@@ -85,7 +85,7 @@ ses <- SESraster(r, FUN = appsv,
                     aleats = 5)
 plot(ses)
 
-## ---- ses-trait2, fig.height=4, fig.width=4-----------------------------------
+## ----ses-trait2, fig.height=4, fig.width=4------------------------------------
 set.seed(10)
 ses <- SESraster(r, FUN = appsv,
                  FUN_args = list(lyrv = trait, na.rm = TRUE),
--- inst/doc/spatial-null-models.R
+++ inst/doc/spatial-null-models.R
@@ -1,17 +1,17 @@
-## ---- include = FALSE---------------------------------------------------------
+## ----include = FALSE----------------------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
   comment = "#>"
 )
 
-## ---- eval = FALSE------------------------------------------------------------
+## ----eval = FALSE-------------------------------------------------------------
 #  install.packages("SESraster")
 
-## ---- eval = FALSE------------------------------------------------------------
+## ----eval = FALSE-------------------------------------------------------------
 #  require(devtools)
 #  devtools::install_github("HemingNM/SESraster", build_vignettes = TRUE)
 
-## ---- rand-spp, fig.height=8, fig.width=6-------------------------------------
+## ----rand-spp, fig.height=8, fig.width=6--------------------------------------
 library(SESraster)
 library(terra)
 # creating random species distributions
@@ -30,33 +30,33 @@ r <- rast(lapply(1:18,
 names(r) <- paste("sp", 1:nlyr(r))
 plot(r)
 
-## ---- naive-both, fig.height=8, fig.width=6-----------------------------------
+## ----naive-both, fig.height=8, fig.width=6------------------------------------
 srb <- bootspat_naive(r, random = "both")
 plot(srb, legend=F)
 
-## ---- naive-both-rich, fig.height=4, fig.width=6------------------------------
+## ----naive-both-rich, fig.height=4, fig.width=6-------------------------------
 plot(c(sum(r), sum(srb)), main=c("observed", "randomized"))
 
-## ---- naive-spp, fig.height=8, fig.width=6------------------------------------
+## ----naive-spp, fig.height=8, fig.width=6-------------------------------------
 sr1 <- bootspat_naive(r, random = "species")
 plot(sr1, legend=F)
 
-## ---- naive-spp-rich, fig.height=4, fig.width=6-------------------------------
+## ----naive-spp-rich, fig.height=4, fig.width=6--------------------------------
 plot(c(sum(r), sum(sr1)), main=c("observed", "randomized"))
 
-## ---- naive-sppb, fig.height=8, fig.width=6-----------------------------------
+## ----naive-sppb, fig.height=8, fig.width=6------------------------------------
 sr1b <- bootspat_naive(r, random = "species", memory = FALSE)
 
-## ---- table-freq-naive--------------------------------------------------------
+## ----table-freq-naive---------------------------------------------------------
 cbind(observed=sapply(r, function(x)freq(x)[2,3]),
       randomized=sapply(sr1, function(x)freq(x)[2,3]),
       randomized_freq=sapply(sr1b, function(x)freq(x)[2,3]))
 
-## ---- naive-site, fig.height=8, fig.width=6-----------------------------------
+## ----naive-site, fig.height=8, fig.width=6------------------------------------
 sr2 <- bootspat_naive(r, random = "site")
 plot(sr2, legend=F)
 
-## ---- naive-site-rich, fig.height=4, fig.width=6------------------------------
+## ----naive-site-rich, fig.height=4, fig.width=6-------------------------------
 plot(c(sum(r), sum(sr2)), main=c("observed", "randomized"))
 
 ## -----------------------------------------------------------------------------
@@ -69,13 +69,13 @@ prob <- terra::app(r,
 
 randr10 <- bootspat_str(r, rprob = prob, fr_prob = fr.prob)
 
-## ---- str-site, fig.height=8, fig.width=6-------------------------------------
+## ----str-site, fig.height=8, fig.width=6--------------------------------------
 plot(randr10, legend=F)
 
-## ---- str-site-rich, fig.height=4, fig.width=6--------------------------------
+## ----str-site-rich, fig.height=4, fig.width=6---------------------------------
 plot(c(sum(r), sum(randr10)), main=c("observed", "randomized"))
 
-## ---- table-freq--------------------------------------------------------------
+## ----table-freq---------------------------------------------------------------
 cbind(observed=sapply(r, function(x)freq(x)[2,3]),
       randomized=sapply(randr10, function(x)freq(x)[2,3]))
 
@@ -83,7 +83,7 @@ cbind(observed=sapply(r, function(x)freq(x)[2,3]),
 # bootstrapping once
 randff <- bootspat_ff(r)
 
-## ---- str-ff, fig.height=4, fig.width=6---------------------------------------
+## ----str-ff, fig.height=4, fig.width=6----------------------------------------
 obs_fr <- unlist(terra::global(r, function(x) sum(x, na.rm = TRUE)))
 v_seq <- order(obs_fr)
 plot(c(r[[v_seq[length(v_seq)-1:4]]], randff[[v_seq[length(v_seq)-1:4]]]), 
@@ -94,10 +94,10 @@ plot(c(r[[v_seq[4:1]]], randff[[v_seq[4:1]]]),
      nr=2, main=paste(rep(c("original", "null"), each=4), names(r[[v_seq[4:1]]])))
 
 
-## ---- str-ff-rich, fig.height=4, fig.width=6----------------------------------
+## ----str-ff-rich, fig.height=4, fig.width=6-----------------------------------
 plot(c(sum(r), sum(randff), sum(r)-sum(randff)), main=c("observed", "randomized", "difference"), nr=1)
 
-## ---- table-freqff------------------------------------------------------------
+## ----table-freqff-------------------------------------------------------------
 cbind(observed=sapply(r, function(x)freq(x)[2,3]),
       randomized=sapply(randff, function(x)freq(x)[2,3]))
 
